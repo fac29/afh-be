fd25bb24a7171e7f2bbe42ff6e3caf36996fce47
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-07-03 17:00:41 +0100

    refactoring Collection table with relationships

--- Models/Collection.cs
+++ Models/Collection.cs
@@ -2,15 +2,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace afh_be.Models
 {
+    // public class Collection
+    // {
+    //     [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    //     public int ID { get; set; }
+    //     public string Title { get; set; }
+    //     public string Description { get; set; }
+    //     public DateTime CreatedAt { get; set; }
+    //     public int UserID { get; set; }
+
+    //     public User User { get; set; }
+    // }
+
     public class Collection
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public int UserID { get; set; }
 
+        // many-to-many relationship with Movie
+        public ICollection<Movie> Movies { get; set; }
+        // many-to-one relationship with User
         public User User { get; set; }
     }
 }
