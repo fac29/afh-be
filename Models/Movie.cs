13922772c46263b1c8101e53c2c9468e562ef244
Alex <alexperezdavies@gmail.com>
2024-07-08 16:21:06 +0100

    user and movie id autoincrements when adding new one

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,18 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace afh_be.Models
 {
     public class Movie
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MovieID { get; set; }
-        public string Title { get; set; }
-        public string Length { get; set; }
-        public string Description { get; set; }
-        public string Genre { get; set; }
-        public string Image { get; set; }
+
+        public string? Title { get; set; }
+        public string? Length { get; set; }
+        public string? Description { get; set; }
+        public string? Genre { get; set; }
+        public string? Image { get; set; }
         public int Rating { get; set; }
         public DateTime CreatedAt { get; set; }
-        public int UserID { get; set; }
     }
 }
