055f0bb26f13494b54a4a7e7775f4b171c2f10da
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-09 16:24:29 +0100

    Movie Model

---
+++ afh-db/Models/Movie.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace afh_db.Models
+{
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public int MovieID { get; set; }
+
+        public string? Title { get; set; }
+        public string? Length { get; set; }
+        public string? Description { get; set; }
+        public string? Genre { get; set; }
+        public string? Image { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
