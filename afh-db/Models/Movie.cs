8909052c325939755176adedd4d1366ed0d4cb9c
Alex <alexperezdavies@gmail.com>
2024-07-10 16:17:53 +0100

    Join collection and movie models

--- afh-db/Models/Movie.cs
+++ afh-db/Models/Movie.cs
@@ -5,19 +5,18 @@ using System.Text.Json.Serialization;
 
 namespace afh_db.Models
 {
-    public class Movie
+  public class Movie
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [JsonIgnore]
         public int MovieID { get; set; }
-
         public string? Title { get; set; }
         public string? Length { get; set; }
         public string? Description { get; set; }
         public string? Genre { get; set; }
-        public string? Image { get; set; }
+        public string? Image { get; set; }        
         public int Rating { get; set; }
         public DateTime CreatedAt { get; set; }
+        public ICollection<CollectionMovie>? CollectionMovies { get; set; }
     }
 }
