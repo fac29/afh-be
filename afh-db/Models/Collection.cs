d0cd07634660b76ae8792c87823d353103f3f5c2
Alex <alexperezdavies@gmail.com>
2024-07-10 16:16:09 +0100

    Create collection model

---
+++ afh-db/Models/Collection.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace afh_db.Models
+{
+    public class Collection
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CollectionID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserID { get; set; }
+        public User? User { get; set; }
+        public ICollection<CollectionMovie>? CollectionMovies { get; set; }
+    }
+}
