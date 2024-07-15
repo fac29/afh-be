d3816b645a23c02b117f6278de19ec3083284263
Alex <alexperezdavies@gmail.com>
2024-07-15 15:21:20 +0100

    add movie fto to get and get by id endpoints

--- afh-api/Controllers/MovieController.cs
+++ afh-api/Controllers/MovieController.cs
@@ -2,7 +2,10 @@
 using afh_db;
 using afh_db.Libraries;
 using afh_db.Models;
+using afh_api.DTOs;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace afh_be.Controllers
@@ -12,19 +15,25 @@ namespace afh_be.Controllers
     public class MovieController : ControllerBase
     {
         private readonly IMovieLibrary _movieLibrary;
+        private readonly IMapper _mapper;
 
-        public MovieController(IMovieLibrary movieLibrary)
+        public MovieController(IMovieLibrary movieLibrary, IMapper mapper)
         {
             _movieLibrary = movieLibrary;
+            _mapper = mapper;
         }
 
         [HttpGet]
+        [SwaggerResponse(200, "Success", typeof(MovieDto))]
         public async Task<ActionResult<IEnumerable<Movie>>> GetMoviesList()
         {
-            return await _movieLibrary.GetMoviesList();
+            var movies = await _movieLibrary.GetMoviesList();
+            var movieDto = _mapper.Map<MovieDto>(movies);
+            return Ok(movieDto);
         }
 
         [HttpGet("{id}")]
+        [SwaggerResponse(200, "Success", typeof(MovieDto))]
         public async Task<ActionResult<Movie>> GetMovieById(int id)
         {
             var movie = await _movieLibrary.GetMovieById(id);
@@ -34,7 +43,9 @@ namespace afh_be.Controllers
                 return NotFound();
             }
 
-            return movie;
+             var movieDto = _mapper.Map<MovieDto>(movie);
+
+            return Ok(movieDto);
         }
 
         [HttpPost("")]
