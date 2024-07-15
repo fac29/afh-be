42c7ef7c824d7bf6ae03be9ee0a04fa1c36282d6
Alex <alexperezdavies@gmail.com>
2024-07-15 15:20:46 +0100

    create movie dto

---
+++ afh-api/DTOs/MovieDto.cs
@@ -0,0 +1,13 @@
+namespace afh_api.DTOs
+{
+  public class MovieDto
+    {
+        public string? Title { get; set; }
+        public string? Length { get; set; }
+        public string? Description { get; set; }
+        public string? Genre { get; set; }
+        public string? Image { get; set; }        
+        public int Rating { get; set; }
+        public ICollection<CollectionMovie>? CollectionMovies { get; set; }
+    }
+}
