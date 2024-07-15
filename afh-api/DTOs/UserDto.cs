6f97de86d96c4da8c39a60d7783980252dfd7471
Alex <alexperezdavies@gmail.com>
2024-07-15 17:21:08 +0100

    Add userid to user dto

--- afh-api/DTOs/UserDto.cs
+++ afh-api/DTOs/UserDto.cs
@@ -2,6 +2,7 @@ namespace afh_api.DTOs
 {
     public class UserDto
     {
+        public int UserID { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
     }
