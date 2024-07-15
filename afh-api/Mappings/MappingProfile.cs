cdd8251043fbe1ab3e303a180063d82573deb9bd
Alex <alexperezdavies@gmail.com>
2024-07-15 10:45:48 +0100

    create mapping profile

---
+++ afh-api/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using afh_api.DTOs;
+using afh_db.Models;
+
+namespace afh_api.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
