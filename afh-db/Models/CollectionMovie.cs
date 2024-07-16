56e07401190b7c7b8c2b7331a21039f96ccde928
Alex <alexperezdavies@gmail.com>
2024-07-16 16:45:14 +0100

    change CollectionMovie model

--- afh-db/Models/CollectionMovie.cs
+++ afh-db/Models/CollectionMovie.cs
@@ -3,8 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 using afh_db.Models;
 public class CollectionMovie
 {
+    [Key]
     public int CollectionID { get; set; }
-    public Collection? Collection { get; set; }
+    public Collection Collection { get; set; }
+    [Key]
     public int MovieID { get; set; }
-    public Movie? Movie { get; set; }
+    public Movie Movie { get; set; }
 }
