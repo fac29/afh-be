809837107e556f5bcffc075624ef5a163299b87f
Alex <alexperezdavies@gmail.com>
2024-07-17 11:40:01 +0100

    Create AddCollectionDto Map

--- afh-api/Mappings/MappingProfile.cs
+++ afh-api/Mappings/MappingProfile.cs
@@ -15,6 +15,9 @@ namespace afh_api.Mappings
             CreateMap<AddMovieDto, Movie>();
             CreateMap<Collection, CollectionDto>()
             .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.CollectionMovies.Select(cm => cm.Movie)));
+            CreateMap<AddCollectionDto, Collection>()
+            .ForMember(dest => dest.CollectionMovies, opt => opt.MapFrom(src => src.CollectionMovies.Select(m => new CollectionMovie { MovieID = m.MovieID })))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
         }
     }
 }
