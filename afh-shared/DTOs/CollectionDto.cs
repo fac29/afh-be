9d274b210ca1eb9a21203ee8521a599a5534eb15
Alex <alexperezdavies@gmail.com>
2024-07-17 11:39:47 +0100

    Create AddCollectionDto

--- afh-shared/DTOs/CollectionDto.cs
+++ afh-shared/DTOs/CollectionDto.cs
@@ -10,4 +10,11 @@ namespace afh_shared.DTOs
         public int UserID { get; set; }
         public List<MovieDto> Movies { get; set; } = new List<MovieDto>();
     }
+
+     public class AddCollectionDto {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int UserID { get; set; }
+        public List<MovieDto> CollectionMovies { get; set; } = new List<MovieDto>();
+    }
 }
